439852947b66e77ccf8ab8bd272fdca82ae52297
Mousstoifa <138684676+Mousstoifa@users.noreply.github.com>
2025-02-26 03:44:41 +0100

    Delete Anybuddy_Backend directory

--- Anybuddy_Backend/Src/Routes/JoueurRoutes.js
+++
@@ -1,9 +0,0 @@
-import express from 'express';
-import { addJoueurToEquipe } from '../Controllers/JoueurControllers.js';
-
-const router = express.Router();
-
-// Ajouter un joueur à une équipe
-router.post('/:idEquipe/joueurs', addJoueurToEquipe);
-
-export default router;
--- Anybuddy_Backend/Src/Utils/Supabase.js
+++
@@ -1,7 +0,0 @@
-import { supabase } from './Src/Utils/Supabase';
-
-export const fetchUsers = async () => {
-  const { data, error } = await supabase.from('users').select('*');
-  if (error) throw new Error(error.message);
-  return data;
-};
