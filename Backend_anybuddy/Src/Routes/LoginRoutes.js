54db372af238be1922d0255e70b2e3a83cda01d5
Mousstoifa <138684676+Mousstoifa@users.noreply.github.com>
2025-02-28 03:30:16 +0100

    Delete Backend_anybuddy directory

--- Backend_anybuddy/Src/Routes/LoginRoutes.js
+++
@@ -1,9 +0,0 @@
-import express from 'express';
-import { loginUser } from '../Controllers/LoginControllers.js';
-
-const router = express.Router();
-
-router.post('/', loginUser);
-
-export default router;
-
